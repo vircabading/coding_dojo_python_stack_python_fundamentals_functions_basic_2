5bd29d170c5daa76468ce0269af4e886a9cc7818
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-25 21:32:01 -0700

    add function 3. first plus length

--- functions_basic_2.py
+++ functions_basic_2.py
@@ -29,10 +29,18 @@ def print_and_return(number_list):
 
 print("Number returned from print_and_return([1,2]) is", print_and_return([1,2]))
 
-"""
 # /////////////////////////////////////////////////////////////
-print_desc("3.")
+print_desc("3. First Plus Length")
 
+def first_plus_length (number_list):
+    sum = 0;
+    if len(number_list) > 0 :
+        sum += number_list[0]
+    return sum + len(number_list)
+
+print ("first_plus_length of [1,2,3,4,5] is", first_plus_length([1,2,3,4,5]))
+
+"""
 # /////////////////////////////////////////////////////////////
 print_desc("4.")
 
